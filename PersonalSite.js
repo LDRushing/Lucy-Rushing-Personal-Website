7332c629ec68824b29a70bfa1f13517897210eed
LDRushing <rushinglucy@yahoo.com>
2023-09-25 22:26:00 -0500

    GoodDay

--- PersonalSite.js
+++ PersonalSite.js
@@ -1,3 +1,15 @@
 //Connect this JS with my HTMLs with 'Hello World!', as explained by Liz. 
 console.log('Hello World!')
+//Make the Home link functional with a 'function' string. 
+onClick: function (HOME) {
+let document.link("Personal1Home.html");
+   }
+//Make the About link functional with a 'function' string. 
+onClick: function (ABOUT) {
+    let document.link("Personal2About.html")
+   }
+//Make the Portfolio link functional with a 'function' string. 
+onClick: function (PORTFOLIO) {
+    let document.link("Personal3Portfolio.html")
+   }
 //Done! :) 
